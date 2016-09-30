b29374e7108575692bdb436a6a5d38fb9661a198
Mikhail Rybin (JangoFettHD) <jangofetthd@gmail.com>
2016-09-30 18:09:43 +0800

    add new futures

--- adressbook.py
+++ adressbook.py
@@ -3,9 +3,9 @@ import json
 
 # initialize
 print("------Справочник 0.01a------")
-# db_directory="/media/student/JF-MINI/Book/db.txt" #linux
-db_directory = "E:\\Book\\Win\\db.txt"  # win
-db_temp = "E:\\Book\\Win\\db_temp.txt"  # win
+# db_directory = "/storage/emulated/0/Download/AdressBook_PY-master/AdressBook_PY-master/db.txt"  # linux
+db_directory = "db.txt"  # win
+# db_temp = "E:\\Book\\Win\\db_temp.txt"  # win
 db = {}
 
 
@@ -17,7 +17,7 @@ def init():
         if not (len(content) == 0):
             json1_data = json.loads(content)
             db = json1_data
-            print('\x1b[6;30;42m'+"@LOG: "+str(db) + '\x1b[0m')
+            print('\x1b[6;30;42m' + "@LOG: " + str(db) + '\x1b[0m')
 
 
 def add():
@@ -80,14 +80,14 @@ def add():
 
     f = open(db_directory, "r+")
     # init()
-    print('\x1b[6;30;42m'+"@LOG: "+"@LOG: "+str(db)+ '\x1b[0m')
+    print('\x1b[6;30;42m' + "@LOG: " + "@LOG: " + str(db) + '\x1b[0m')
     if "contact" in db.keys():
         db["contact"].append(dct)
     else:
         db["contact"] = [dct]
 
     # db["contact"]=dct
-    print('\x1b[6;30;42m'+"@LOG: "+"@LOG: "+str(db)+ '\x1b[0m')
+    print('\x1b[6;30;42m' + "@LOG: " + "@LOG: " + str(db) + '\x1b[0m')
 
     temp = str(db)
     str_dct = temp.replace("\'", "\"")
@@ -97,6 +97,76 @@ def add():
     f.close()
 
 
+def removeJson(index):
+    global db
+    init()
+    print(db["contact"])
+    db["contact"].pop(index - 1)
+
+    f = open(db_directory, "r+")
+    f.seek(0)
+    f.truncate()
+    temp = str(db)
+    str_dct = temp.replace("\'", "\"")
+    f.write(str_dct)
+    f.close()
+    print(db["contact"])
+
+
+def editJson(index, element):
+    global db
+    init()
+    print(db["contact"])
+
+    print("EDIT")
+
+    print("===Введите новое значение===")
+    if (element < 6):
+        inp = input()
+    else:
+        inp = int(input())
+
+    if element == 1:
+        print("1-фамилия")
+        db["contact"][index - 1]["surname"] = inp
+    if element == 2:
+        print("2-имя")
+        db["contact"][index - 1]["name"] = inp
+    if element == 3:
+        print("3-отчество")
+        db["contact"][index - 1]["second_name"] = inp
+    if element == 4:
+        print("4-город")
+        db["contact"][index - 1]["city"] = inp
+    if element == 5:
+        print("5-улица")
+        db["contact"][index - 1]["street"] = inp
+    if element == 6:
+        print("6-дом")
+        db["contact"][index - 1]["house"] = inp
+    if element == 7:
+        print("7-номер квартиры")
+        db["contact"][index - 1]["room"] = inp
+    if element == 8:
+        print("8-номер домашнего телефона")
+        db["contact"][index - 1]["telephone"] = inp
+    if element == 9:
+        print("9-номер сотового телефона ")
+        db["contact"][index - 1]["number"] = inp
+
+    ###
+
+    f = open(db_directory, "r+")
+    f.seek(0)
+    f.truncate()
+    temp = str(db)
+    str_dct = temp.replace("\'", "\"")
+    f.write(str_dct)
+    f.close()
+    print(db["contact"])
+
+
+'''
 def remove(line_index):
     lines = []
     with open(db_directory, "r") as f:
@@ -106,6 +176,58 @@ def remove(line_index):
     with open(db_directory, "w") as f:
         for l in lines:
             f.write(l)
+'''
+
+
+def searchJson_city(s1):
+    global db
+    init()
+    print("===Результат поиска===")
+    i = 0
+    while i < len(db["contact"]):
+        if s1.lower() in str(db["contact"][i]["city"]).lower():
+            print(str(i + 1) + ") " + str(db["contact"][i]["surname"]) + " " + str(
+                db["contact"][i]["name"] + " " + db["contact"][i]["second_name"]) + ", город " + str(
+                db["contact"][i]["city"]) + ", улица " + str(db["contact"][i]["street"]) + " " + str(
+                db["contact"][i]["house"]) + " " + str(db["contact"][i]["room"]) + ", тел: " + str(
+                db["contact"][i]["number"]) + ", дом.тф: " + str(db["contact"][i]["telephone"]))
+        i = i + 1
+    print("======================")
+
+
+def searchJson_street(s1, s2):
+    global db
+    init()
+    print("===Результат поиска===")
+    i = 0
+    while i < len(db["contact"]):
+        if s1.lower() in str(db["contact"][i]["city"]).lower():
+            if s2.lower() in str(db["contact"][i]["street"]).lower():
+                print(str(i + 1) + ") " + str(db["contact"][i]["surname"]) + " " + str(
+                    db["contact"][i]["name"] + " " + db["contact"][i]["second_name"]) + ", город " + str(
+                    db["contact"][i]["city"]) + ", улица " + str(db["contact"][i]["street"]) + " " + str(
+                    db["contact"][i]["house"]) + " " + str(db["contact"][i]["room"]) + ", тел: " + str(
+                    db["contact"][i]["number"]) + ", дом.тф: " + str(db["contact"][i]["telephone"]))
+        i = i + 1
+    print("======================")
+
+
+def searchJson_house(s1, s2, s3):
+    global db
+    init()
+    print("===Результат поиска===")
+    i = 0
+    while i < len(db["contact"]):
+        if s1.lower() in str(db["contact"][i]["city"]).lower():
+            if s2.lower() in str(db["contact"][i]["street"]).lower():
+                if str(s3) in str(db["contact"][i]["house"]).lower():
+                    print(str(i + 1) + ") " + str(db["contact"][i]["surname"]) + " " + str(
+                        db["contact"][i]["name"] + " " + db["contact"][i]["second_name"]) + ", город " + str(
+                        db["contact"][i]["city"]) + ", улица " + str(db["contact"][i]["street"]) + " " + str(
+                        db["contact"][i]["house"]) + " " + str(db["contact"][i]["room"]) + ", тел: " + str(
+                        db["contact"][i]["number"]) + ", дом.тф: " + str(db["contact"][i]["telephone"]))
+        i = i + 1
+    print("======================")
 
 
 def search(s):
@@ -123,28 +245,35 @@ def info():
     txt1 = "1-добавить запись"
     txt2 = "2-показать справочник"
     txt3 = "3-удалить запись"
-    txt4 = "4-поиск"
+    txt4 = "4-редактировать запись"
+    txt5 = "5-поиск"
     txt0 = "0-выйти"
-    print(txt1 + "\n" + txt2 + "\n" + txt3 + "\n" + txt4 + "\n" + txt0)
+    print(txt1 + "\n" + txt2 + "\n" + txt3 + "\n" + txt4 + "\n" + txt5 + "\n" + txt0)
 
 
 def info_search():
     print("===Поиск===")
     txt1 = "1-по городу"
     txt2 = "2-по улице"
-    txt3 = "3-по фамилии"
+    txt3 = "3-по дому"
     txt0 = "0-назад"
     print(txt1 + "\n" + txt2 + "\n" + txt3 + "\n" + txt0)
 
 
-'''
-def show():
-    print("=========СПРАВОЧНИК=========")
-    t = open(db_directory, "r+")
-    print(t.read())
-    t.close()
-    print("============================")
-    '''
+def info_edit():
+    print("===Выберите элемент для редактирования===")
+    txt1 = "1-фамилия"
+    txt2 = "2-имя"
+    txt3 = "3-отчество"
+    txt4 = "4-город"
+    txt5 = "5-улица"
+    txt6 = "6-дом"
+    txt7 = "7-номер квартиры"
+    txt8 = "8-номер домашнего телефона"
+    txt9 = "9-номер сотового телефона "
+    txt0 = "0-назад"
+    print(txt1 + "\n" + txt2 + "\n" + txt3 + "\n" + txt4 + "\n" + txt5 + "\n" +
+          txt6 + "\n" + txt7 + "\n" + txt8 + "\n" + txt9 + "\n" + txt0)
 
 
 def show():
@@ -153,8 +282,12 @@ def show():
     if len(db) > 0:
         i = len(db["contact"])
         while i > 0:
-            #print(db["contact"][i - 1]["name"])
-            print(str(db["contact"][i - 1]["surname"])+" "+str(db["contact"][i - 1]["name"]+" "+db["contact"][i - 1]["second_name"])+", город "+str(db["contact"][i - 1]["city"])+", улица "+str(db["contact"][i - 1]["street"])+" "+str(db["contact"][i - 1]["house"])+" "+str(db["contact"][i - 1]["room"])+", тел: "+str(db["contact"][i - 1]["number"])+", дом.тф: "+str(db["contact"][i - 1]["telephone"]))
+            # print(db["contact"][i - 1]["name"])
+            print(str(i) + ") " + str(db["contact"][i - 1]["surname"]) + " " + str(
+                db["contact"][i - 1]["name"] + " " + db["contact"][i - 1]["second_name"]) + ", город " + str(
+                db["contact"][i - 1]["city"]) + ", улица " + str(db["contact"][i - 1]["street"]) + " " + str(
+                db["contact"][i - 1]["house"]) + " " + str(db["contact"][i - 1]["room"]) + ", тел: " + str(
+                db["contact"][i - 1]["number"]) + ", дом.тф: " + str(db["contact"][i - 1]["telephone"]))
             i = i - 1
     print("============================")
 
@@ -164,23 +297,77 @@ def clear():
 
 
 # programm
+init()
 info()
 i = -1
 while int(i) != 0:
-    i = int(input())
-    clear()
+    try:
+        i = int(input())
+        clear()
+    except ValueError:
+        print('\033[91m' + "Нужно вводить число, а не букву!" + '\033[0m')
     if int(i) == 2:
         show()
     if int(i) == 1:
         add()
     if int(i) == 3:
-        print("===Введите номер строки для удаления===")
-        b = int(input())
-        remove(b)
+        e = -1
+        while int(e) != 0:
+            print("===Введите индекс контакта для удаления===")
+            print("* или введите 0, чтобы вернуться назад")
+            try:
+                e = int(input())
+                clear()
+            except ValueError:
+                print('\033[91m' + "Нужно вводить число, а не букву!" + '\033[0m')
+            if e > 0 and e < (len(db["contact"]) + 1):
+                removeJson(e)
+                e = 0
     if int(i) == 4:
-        print("===Введите слово для поиска===")
-        inp1 = input()
-        search(inp1)
+        e = -1
+        while int(e) != 0:
+            print("===Введите индекс контакта для редактирования===")
+            print("* или введите 0, чтобы вернуться назад")
+            try:
+                e = int(input())
+                clear()
+            except ValueError:
+                print('\033[91m' + "Нужно вводить число, а не букву!" + '\033[0m')
+            if e > 0 and e < (len(db["contact"]) + 1):
+                e = 0  # выход к перд.меню
+                z = -1
+                while int(z) != 0:
+                    info_edit()
+                    try:
+                        z = int(input())
+                        clear()
+                    except ValueError:
+                        print('\033[91m' + "Нужно вводить число, а не букву!" + '\033[0m')
+                    if z > 0:
+                        editJson(e, int(z))
+    if int(i) == 5:
+        o = -1
+        while int(o) != 0:
+            info_search()
+            o = int(input())
+            if int(o) == 1:
+                print("===Введите название города===")
+                ss = input()
+                searchJson_city(ss)
+            if int(o) == 2:
+                print("===Введите название города===")
+                s1 = input()
+                print("===Введите название улицы===")
+                s2 = input()
+                searchJson_street(s1, s2)
+            if int(o) == 3:
+                print("===Введите название города===")
+                s1 = input()
+                print("===Введите название улицы===")
+                s2 = input()
+                print("===Введите номер дома===")
+                s3 = input()
+                searchJson_house(s1, s2, s3)
     if int(i) == 5:
         init()
     info()
